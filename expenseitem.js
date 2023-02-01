3ace61eb4d1f8ccc42aff562d29dac6a800b9f63
toofanjhala <121038143+toofanjhala@users.noreply.github.com>
2023-02-01 19:20:28 +0530

    props

--- expenseitem.js
+++ expenseitem.js
@@ -1,10 +1,14 @@
-function Expenseitem() {
+import './expenseitem.css'
+function Expenseitem(props) {
+
     return (
-        <div>
-            <h2>Expenseitem!</h2>
-            <h4>food rs 100</h4>
-            <h4>petrol rs 100</h4>
-            <h4>movies rs 200</h4>
+        <div className='expense-item'>
+            <div>{props.date.toDateString()}</div>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <h4>{props.destination}</h4>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
         </div>
     )
 }
