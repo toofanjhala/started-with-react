6ddbbe051d2dd0c5648f47f374aba9c438a7c3ea
toofanjhala <121038143+toofanjhala@users.noreply.github.com>
2023-02-01 16:50:50 +0530

    create a component expenseitem and export

---
+++ App.js
@@ -0,0 +1,12 @@
+import Expenseitem from "./components/expenseitem";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenseitem></Expenseitem>
+    </div>
+  );
+}
+
+export default App;
---
+++ expenseitem.js
@@ -0,0 +1,12 @@
+function Expenseitem() {
+    return (
+        <div>
+            <h2>Expenseitem!</h2>
+            <h4>food rs 100</h4>
+            <h4>petrol rs 100</h4>
+            <h4>movies rs 200</h4>
+        </div>
+    )
+}
+
+export default Expenseitem;
