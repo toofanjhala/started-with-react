7c4c2e4bf800f0a9084f48d0b23ff093c74acb15
toofanjhala <121038143+toofanjhala@users.noreply.github.com>
2023-02-01 21:16:48 +0530

    use map method with props

--- App.js
+++ App.js
@@ -25,31 +25,34 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+
+
+  
+
   return (
+   
+
     <div>
-      <h2>Let's get started!</h2>
+    <h2>Let's get started!</h2>
 
-      <Expenseitem title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-        destination={expenses[0].destination}> </Expenseitem>
-      <Expenseitem title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-        destination={expenses[1].destination} ></Expenseitem>
-         <Expenseitem title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-        destination={expenses[2].destination} ></Expenseitem>
-         <Expenseitem title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-        destination={expenses[3].destination} ></Expenseitem>
-
-
-
+      {expenses.map((Expens) => {
+        return (
+        <div>
+          <Expenseitem title={Expens.title}
+            amount={Expens.amount}
+            date={Expens.date}
+            destination={Expens.destination}> </Expenseitem>
+        </div>
+        )
+      })}
     </div>
-  );
+
+  )
+
 }
+
+
+
 
 export default App;
